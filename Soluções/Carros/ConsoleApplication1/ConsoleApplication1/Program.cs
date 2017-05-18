03cfbea2d75b6bddd782eb55a532b639b70770c1
ElGatoDeCartola <dhiogo.martins.oliveira@gmail.com>
2017-05-18 13:35:07 -0300

    Trabalhos em geral

---
+++ Soluções/Carros/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public struct Carro
+        {
+            public string modelo;
+            public double cv;
+            public double km;
+        }
+
+        static void Main(string[] args)
+        {
+             Carro Car;
+
+            Console.WriteLine("Quantidade de carros");
+            int N = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Car.modelo = Console.ReadLine();
+                Car.km = Double.Parse(Console.ReadLine());
+                Car.cv = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine(Classificar(Car));
+            }
+        }
+
+        public static string Classificar(Carro c)
+        {
+            string pot, uso;
+
+            if (c.km <= 5000)
+                uso = "Novo";
+            else if (c.km <= 30000)
+                uso = "Seminovo";
+            else
+                uso = "Velho";
+
+            if (c.cv < 120)
+                pot = "Popular";
+            else if (c.cv < 200)
+                pot = "Forte";
+            else
+                pot = "Potente";
+
+            return String.Format("{0} - {1} - {2}", c.modelo, uso, pot); 
+
+        }
+     }
+}
---
+++ Soluções/Exercicios 18-5-2017/Exercicios 18-5-2017/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercicios_18_5_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
