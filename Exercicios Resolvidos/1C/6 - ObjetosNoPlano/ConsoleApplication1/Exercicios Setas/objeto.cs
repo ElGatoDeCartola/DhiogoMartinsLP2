2fc0fec50ae72b6bb799a2f6c39f759193158bac
Dhiogo Martins <dhiogo.martins.oliveira@gmail.com>
2017-07-12 05:00:48 -0300

    Organização

---
+++ Exercicios Resolvidos/1C/6 - ObjetosNoPlano/ConsoleApplication1/Exercicios Setas/objeto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Objeto2D
+    {
+        public int X;
+        public int Y;
+        public void AndarParaADireita()
+        {
+            X++;
+        }
+        public void AndarParaAEsquerda()
+        {
+            X--;
+        }
+        public void AndarParaCima()
+        {
+            Y++;
+        }
+        public void AndarParaBaixo()
+        {
+            Y--;
+        }
+        public string Coordenadas()
+        {
+            return String.Format("({0} {1})" ,Y , X );
+        }
+    }
+}
---
+++ Exercicios Resolvidos/1C/7 - CompetiçaoCorrida/Corrida/Corrida/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corrida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            
+            Console.WriteLine("Qual o número de competidores?");
+            n = int.Parse(Console.ReadLine());
+
+            double t1,t2,t3;
+            double menorTempo = double.MaxValue;
+            string vencedor = "";
+
+            Competidor[] competidores = new Competidor[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                competidores[i] = new Competidor();
+
+                Console.WriteLine("\nInsira o nome do competidor {0}", i+1);
+                competidores[i].name = Console.ReadLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nInsira o 1º tempo do competidor {0}", competidores[i].name);
+                competidores[i].t1 = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nInsira o 2º tempo do competidor {0}", competidores[i].name);
+                competidores[i].t2 = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nInsira o 3º tempo do competidor {0}", competidores[i].name);
+                competidores[i].t3 = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (competidores[i].calte() < menorTempo)
+                {
+                    menorTempo = competidores[i].calte();
+
+                    vencedor = competidores[i].name;
+                }
+            }
+
+            Console.WriteLine("\n{0}", vencedor);
+
+        }
+    }
+}
