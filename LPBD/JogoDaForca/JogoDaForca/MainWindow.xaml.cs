779593065a0af634961ad50c6b0db22e31cb8e91
Dhiogo Martins <dhiogo.martins.oliveira@gmail.com>
2017-12-05 17:53:08 -0300

    Novos Exercicios LP

---
+++ LPBD/JogoDaForca/JogoDaForca/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JogoDaForca
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Palavra_Click(object sender, RoutedEventArgs e)
+        {
+            string TxtPalavra = txt_Palavra.Text;
+            int TxtTamanho = txt_Palavra.Text.Length;
+            string block = "";
+            string vazio = "";
+            for (int i = 0; i < TxtTamanho; i++)
+            {
+                if (TxtPalavra[i] != vazio[0])
+                {
+
+                }
+                block = block + "*";
+            }
+            txtblock_Palavra.Text = block;
+        }
+
+        private void txt_Palavra_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            txt_Palavra.Clear();
+        }
+
+        private void txt_Letra_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            txt_Letra.Clear();
+        }
+    }
+}
---
+++ LPBD/WPF/WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnmostrartamanho_Click(object sender, RoutedEventArgs e)
+        {
+            string s = txtcaixa.Text;
+            int tamanho = s.Replace(" ", "").Length;
+            string resposta = string.Format("O texto tem {0} caracteres" , tamanho);
+            MessageBox.Show(Convert.ToString(txtcaixa.Text.Length));
+            MessageBox.Show(String.Format("O Texto tem {0} caracteres.", txtcaixa.Text.Length));
+        }
+
+        private void btnApagar_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
